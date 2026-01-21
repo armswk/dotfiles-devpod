3244d7f8520d7237143e48d23adf8e612534faa0
armswk <siwakorn.s@yahoo.com>
2026-01-21 15:36:59 +0000

    add init.lua

---
+++ dot_config/nvim/lua/init.lua
@@ -0,0 +1,16 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+vim.g.clipboard = {
+	name = "OSC 52",
+	copy = {
+		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+	},
+	paste = {
+		["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+		["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+	},
+}
+
+-- Optional: Sync with system clipboard automatically
+vim.opt.clipboard = "unnamedplus"
