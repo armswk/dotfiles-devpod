c7adbf03c04e6b6eca2eceb5856f48ea77b1ccef
armswk_ <siwakorn.s@yahoo.com>
2026-03-05 15:19:04 +0000

    add nvim plugin extras

--- dot_config/nvim/lua/plugins/extras.lua
+++ dot_config/nvim/lua/plugins/extras.lua
@@ -2,4 +2,6 @@ return {
 	{ import = "lazyvim.plugins.extras.lang.json" },
 	{ import = "lazyvim.plugins.extras.lang.toml" },
 	{ import = "lazyvim.plugins.extras.lang.yaml" },
+	{ import = "lazyvim.plugins.extras.dap.core" },
+	{ import = "lazyvim.plugins.extras.lang.python" },
 }
